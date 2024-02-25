521bc7d7e38b1d928bb137e87e001f508920748f
Carlos Martinez <curlosmart@gmail.com>
2024-02-25 11:16:39 -0500

    Added Details and Data Grids (Incomplete for both)

--- src/pages/FocusStatsPage.tsx
+++ src/pages/FocusStatsPage.tsx
@@ -1,15 +1,23 @@
 import { Link } from "react-router-dom";
 import IconsBar from "../components/IconsBar";
 import { FaEllipsisH } from "react-icons/fa";
-import { FaChevronLeft, FaArrowUpRightFromSquare, FaArrowUp, FaArrowDown, FaPlus, FaClock, FaChevronRight } from "react-icons/fa6";
+import { FaChevronLeft, FaArrowUpRightFromSquare, FaArrowUp, FaArrowDown, FaPlus, FaClock, FaChevronRight, FaChevronDown } from "react-icons/fa6";
 import TaskList from "../components/TaskList";
 import { useSelector } from "react-redux";
 import { useState } from "react";
+import { Cell, Label, Legend, Pie, PieChart, PolarAngleAxis, PolarGrid, PolarRadiusAxis, Radar, RadarChart, ResponsiveContainer } from "recharts";
+import CalendarHeatmap from 'react-calendar-heatmap';
+import { Tooltip as ReactTooltip } from 'react-tooltip';
+import 'react-calendar-heatmap/dist/styles.css';
+
+const sharedButtonStyle = `py-1 px-4 rounded-3xl cursor-pointer`;
+const selectedButtonStyle = `${sharedButtonStyle} bg-[#3A2D20] text-[#F87A00] font-semibold`;
+const unselectedButtonStyle = `${sharedButtonStyle} text-[#666666]`;
 
 const TopBar: React.FC = () => {
 
     return (
-        <div className="flex justify-between items-center sticky top-0 py-6 bg-black">
+        <div className="flex justify-between items-center sticky top-0 py-6 bg-black z-10">
             {/* <Link to="/focus" className="cursor-pointer">
                 <FaChevronLeft size={'20px'} color={'#999999'} />
             </Link> */}
@@ -103,12 +111,58 @@ const FocusRecordsPreview = () => (
     </div>
 );
 
-const Details = () => {
-    const [selectedButton, setSelectedButton] = useState('stopwatch');
+const data = [
+    { name: 'Hello Mobile', value: 60 },
+    { name: 'Side Projects', value: 19.5 },
+    { name: 'Q Link Wireless', value: 1.5 },
+    { name: 'Unclassified', value: 0.99 },
+];
+const COLORS = ['#0088FE', '#00C49F', '#FFBB28', '#FF8042'];
 
-    const sharedButtonStyle = `py-1 px-4 rounded-3xl cursor-pointer`;
-    const selectedButtonStyle = `${sharedButtonStyle} bg-[#3A2D20] text-[#F87A00] font-semibold`;
-    const unselectedButtonStyle = `${sharedButtonStyle} text-[#666666]`;
+interface ProgressBarProps {
+    data: Array<any>;
+}
+
+const ProgressBar: React.FC<ProgressBarProps> = ({ data }) => {
+    return (
+        <div className="space-y-4">
+            {data.map((item, index) => (
+                <div>
+                    <div className="flex justify-between items-center mb-1">
+                        <div>{item.name}</div>
+                        <div className="text-[#8C8C8C]">{item.value} • {item.percentage}%</div>
+                    </div>
+                    <div key={index} className="w-full rounded-full dark:bg-[#232323]">
+                        <div
+                            className={`${item.color} text-xs font-medium text-blue-100 text-center p-[3px] leading-none rounded-full`}
+                            style={{ width: `${item.percentage}%` }}
+                        />
+                    </div>
+                </div>
+            ))}
+        </div>
+    );
+};
+
+const Details = () => {
+    const [selectedButton, setSelectedButton] = useState('day');
+
+    const PercentageProgess = () => (
+        <div>
+            <div className="flex justify-between items-center">
+                <div>Hello Mobile</div>
+                <div className="text-[#666666]">59h59m • 73.14%</div>
+                <div></div>
+            </div>
+        </div>
+    );
+
+    const progressBarData = [
+        { name: 'Hello Mobile', value: '59h59m', percentage: 73.14, color: 'bg-[#1F67E2]' },
+        { name: 'Side Projects', value: '19h25m', percentage: 23.67, color: 'bg-[#3690E4]' },
+        { name: 'Q Link Wireless', value: '1h38m', percentage: 1.99, color: 'bg-[#E69138]' },
+        { name: 'Unclassified', value: '59m', percentage: 1.2, color: 'bg-[#FF7D00]' },
+    ];
 
     return (
         <div className="bg-[#1A1A1A] rounded-lg px-5 py-6">
@@ -127,6 +181,146 @@ const Details = () => {
                 <div className={selectedButton === 'month' ? selectedButtonStyle : unselectedButtonStyle} onClick={() => setSelectedButton('month')}>Month</div>
                 <div className={selectedButton === 'custom' ? selectedButtonStyle : unselectedButtonStyle} onClick={() => setSelectedButton('custom')}>Custom</div>
             </div>
+
+            <div className="flex justify-center">
+                <PieChart width={600} height={250}>
+                    <Pie
+                        data={progressBarData}
+                        cx={300}
+                        cy={125}
+                        innerRadius={60}
+                        outerRadius={80}
+                        paddingAngle={5}
+                        dataKey="percentage"
+                        label={(entry) => `${entry.value} (${entry.name})`}
+                    >
+                        {data.map((entry, index) => (
+                            <Cell key={`cell-${index}`} fill={COLORS[index % COLORS.length]} stroke="none" />
+                        ))}
+
+                        <Label value="82h1m" position="center" fill="white" className="text-[24px] font-bold" />
+                    </Pie>
+                </PieChart>
+            </div>
+
+            <div className="mt-3">
+                <div className="flex justify-between items-center">
+                    <div className="font-[500]">Focus Ranking</div>
+                    <div className="flex items-center gap-2">
+                        <div className="text-[#A3A3A3]">List</div>
+                        <FaChevronDown size={'16px'} color={'#666666'} />
+                    </div>
+                </div>
+
+                <div className="mt-3 flex flex-col gap-2">
+                    <ProgressBar data={progressBarData} />
+                </div>
+            </div>
+        </div>
+    );
+};
+
+const Trends = () => {
+    const [selectedButton, setSelectedButton] = useState('day');
+
+    return (
+        <div className="bg-[#1A1A1A] rounded-lg px-5 py-6">
+            <div className="flex items-center justify-between">
+                <div className="text-[18px] font-semibold">Trends</div>
+                <div className="flex items-center gap-1">
+                    <FaChevronLeft size={'16px'} color={'#FF7D00'} />
+                    <div>This Week</div>
+                    <FaChevronRight size={'16px'} color={'#FF7D00'} />
+                </div>
+            </div>
+
+            <div className="mt-5 flex justify-center gap-1">
+                <div className={selectedButton === 'day' ? selectedButtonStyle : unselectedButtonStyle} onClick={() => setSelectedButton('day')}>Day</div>
+                <div className={selectedButton === 'week' ? selectedButtonStyle : unselectedButtonStyle} onClick={() => setSelectedButton('week')}>Week</div>
+                <div className={selectedButton === 'month' ? selectedButtonStyle : unselectedButtonStyle} onClick={() => setSelectedButton('month')}>Month</div>
+                <div className={selectedButton === 'custom' ? selectedButtonStyle : unselectedButtonStyle} onClick={() => setSelectedButton('custom')}>Custom</div>
+            </div>
+
+            {/* Recharts chart */}
+            <div>
+                {/* <ResponsiveContainer width="100%" height="100%">
+                    <RadarChart cx="50%" cy="50%" outerRadius="80%" data={data}>
+                        <PolarGrid />
+                        <PolarAngleAxis dataKey="name" />
+                        <PolarRadiusAxis angle={30} domain={[0, 100]} />
+                        <Radar name="Hello Mobile" dataKey="value" stroke="#8884d8" fill="#8884d8" fillOpacity={0.6} />
+                        <Legend />
+                    </RadarChart>
+                </ResponsiveContainer> */}
+            </div>
+
+            <div className="flex justify-between items-center mt-6">
+                <div className="text-[#9F9F9F] text-[18px]">Daily Average</div>
+                <div className="text-white text-[28px]">4h3m</div>
+            </div>
+        </div>
+    );
+};
+
+const DataGrids = () => {
+
+    const today = new Date();
+
+    function getRange(count) {
+        return Array.from({ length: count }, (_, i) => i);
+    }
+
+    function shiftDate(date, numDays) {
+        const newDate = new Date(date);
+        newDate.setDate(newDate.getDate() + numDays);
+        return newDate;
+    }
+
+    function getRandomInt(min, max) {
+        return Math.floor(Math.random() * (max - min + 1)) + min;
+    }
+
+    // Example data format
+    const randomValues = getRange(200).map(index => {
+        return {
+            date: shiftDate(today, -index),
+            count: getRandomInt(1, 3),
+        };
+    });
+
+    return (
+        <div className="bg-[#1A1A1A] rounded-lg px-5 py-6">
+            <div className="flex items-center justify-between">
+                <div className="text-[18px] font-semibold">Year Grids</div>
+                <div className="flex items-center gap-1">
+                    <FaChevronLeft size={'16px'} color={'#FF7D00'} />
+                    <div>2024</div>
+                    <FaChevronRight size={'16px'} color={'#FF7D00'} />
+                </div>
+            </div>
+
+            <div className="p-3">
+                <CalendarHeatmap
+                    startDate={shiftDate(today, -365)}
+                    endDate={today}
+                    values={randomValues}
+                    classForValue={value => {
+                        if (!value) {
+                            return 'color-empty';
+                        }
+                        return `color-github-${value.count}`;
+                    }}
+                    tooltipDataAttrs={(value) => {
+                        return {
+                            'data-tooltip-id': value ? "my-tooltip" : "",
+                            'data-tooltip-content': value ? `${value.date} has count: ${value.count}` : "",
+                        };
+                    }}
+                    gutterSize={1}
+                    onClick={value => alert(`Clicked on value with count: ${value.count}`)}
+                />
+                <ReactTooltip id="my-tooltip" />
+            </div>
         </div>
     );
 };
@@ -142,6 +336,8 @@ const FocusStatsPage = () => {
                     <BasicFocusStats />
                     <FocusRecordsPreview />
                     <Details />
+                    {/* <Trends /> */}
+                    {/* <DataGrids /> */}
                 </div>
 
             </div>
